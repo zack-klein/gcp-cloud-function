0eb55ee6382cc10e73db1accde7fe1b6630cc00f
zack-klein <klein.zachary.j@gmail.com>
2021-03-15 21:50:37 -0400

    fix: Add service account email to variables

--- variable.tf
+++ variable.tf
@@ -1,49 +1,54 @@
 variable "project_id" {
-    type = string
-    description = "GCP Project ID."
+  type        = string
+  description = "GCP Project ID."
 }
 
 variable "function_name" {
-    type = string
-    description = "The name of the Cloud Function."
+  type        = string
+  description = "The name of the Cloud Function."
 }
 
 variable "entry_point" {
-    type = string
-    description = "The cloud function's entrypoint."
+  type        = string
+  description = "The cloud function's entrypoint."
 }
 
 variable "source_zip" {
-    description = "A zipped up file or directory ready for deployment."
+  description = "A zipped up file or directory ready for deployment."
 }
 
 variable "bucket_name" {
-    type = string
-    description = "Name of the bucket to store the function."
-    default = null
+  type        = string
+  description = "Name of the bucket to store the function."
+  default     = null
 }
 
 variable "description" {
-    type = string
-    description = "Description of the function."
-    default = null
+  type        = string
+  description = "Description of the function."
+  default     = null
 }
 
 variable "runtime" {
-    type = string
-    description = "Runtime environment."
-    default = "python37"
+  type        = string
+  description = "Runtime environment."
+  default     = "python37"
 }
 
 variable "available_memory_mb" {
-    type = number
-    description = "Memory available for the function"
-    default = 256
+  type        = number
+  description = "Memory available for the function"
+  default     = 256
 
 }
 
 variable "trigger_http" {
-    type = bool
-    description = "Can be triggered over HTTP?"
-    default = true
+  type        = bool
+  description = "Can be triggered over HTTP?"
+  default     = true
+}
+
+variable "service_account_email" {
+  description = "Name of the service account to attach to the function (Optional)."
+  default     = null
 }
